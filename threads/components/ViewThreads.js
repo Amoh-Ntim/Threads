748b97d06a848432faedbedd7ca788650f1606ae
Amoh-Ntim <amohntim123@gmail.com>
2024-01-07 04:42:27 +0000

    post successfully retrieved and seen

--- threads/components/ViewThreads.js
+++ threads/components/ViewThreads.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import { Image, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState, useEffect } from 'react';
+import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import tw from 'twrnc';
 
 const ViewThreads = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://192.168.122.69:6000/thread')
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error('Error fetching posts:', error));
+  }, []);
+
   return (
     <SafeAreaView>
     <View style={tw`flex justify-center items-center`}>
         <Image
             source={require('../assets/Vectorlogothreads.png')}
         />
-    </View>
-    <View>
-        <View>
-            
-        </View>
-    </View>
+    </View> 
+      <ScrollView>
+        {posts.map((post) => (
+          <View key={post._id} style={tw`p-4 border-b border-gray-200`}>
+            <Text style={tw`text-lg`}>{post.post}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default ViewThreads
+export default ViewThreads;
+
